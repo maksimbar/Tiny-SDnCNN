12bf53d73b04261acc569c9f241933e3e40a55e6
maksimbar <maksimb@wix.com>
2025-05-14 14:34:55 +0300

    No more determinism

--- train.py
+++ train.py
@@ -281,7 +281,7 @@ def main(config: DictConfig):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.benchmark = True
 
     if config.get("device", "auto") == "auto":
